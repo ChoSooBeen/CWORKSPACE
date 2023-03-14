b0ebed8ef35292c5a3e740acbf8d912c7ab39ecb
ChoSooBeen <cba0613060@gmail.com>
2023-03-14 16:59:25 +0900

    포인터 내용 추가

--- PointerExample/address_of.c
+++ PointerExample/address_of.c
@@ -2,9 +2,15 @@
 
 int main(void){
     int num = 10;
+    int *p = &num;
 
-    //주소 연산자 & 사용
-    printf("num의 주소 : %u", &num);
+    //직접 참조
+    printf("num의 주소 : %u\n", &num);
+    printf("직접 참조 값: %d\n", num);
+
+    //간접 참조
+    printf("num의 주소 : %u\n", p);
+    printf("간접 참조 값: %d\n", *p);
 
     return 0;
 }
